c8112791bf54d010d2ff9bdbefa3a282923871e5
Sidney Rood <116924478+SidRood@users.noreply.github.com>
2023-04-03 21:03:15 -0700

    Add files via upload

---
+++ Assignment5.cs
@@ -0,0 +1,92 @@
+﻿/* Sidney Rood
+ * Assignment 5: Pirate's Plunder - IS350
+ * Program will help a pirate do some accounting and hopefully gain some treasure
+ * There will be 2 simulations, one being indefinitely and another being a set amount of years
+ * Each year pirate has equal chance to gain or lose 25 gold
+ * Game ends when user retires, gold is greater than 1000 or goes down to 0, or number of years is executed
+ */
+
+namespace Assignment5
+{
+    internal class Assignment5
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //create variables
+                int treasure = 0;
+                int year = 1;
+                bool isActive = true;
+
+                Random r = new Random();// random number to determine gold reward
+
+                do
+                {
+                    //get user input for starting amount of treasure
+                    Console.WriteLine("Ahoy mate! What do you want your starting treasure to be? (enter a whole integer between 0 and 1000)");
+                    treasure = Convert.ToInt32(Console.ReadLine());
+                } while (treasure <= 0 || treasure >= 1000);
+
+                Console.WriteLine("You currently have {0} gold. Good luck!", treasure);
+
+                Console.WriteLine("Do you want to experience the years indefinitely (1) or enter an amount of years to find treasure (2) or retire (3)?");
+                int gameType = Convert.ToInt32(Console.ReadLine());
+
+                if (gameType == 1) //game 1 - loop until goldAMount is > 1000 or < 0
+                {
+                    while (treasure >= 0 && treasure <= 1000)
+                    {
+                        if (r.Next(0, 2) == 0)
+                        {
+                            treasure = treasure + 25;//increase gold amount
+                        }
+                        else
+                        {
+                            treasure = treasure - 25;//decrease gold amount
+                        }
+
+                        year++;//increment year
+                               //display updated gold amount plus year
+                        Console.WriteLine("You have {0} gold and it is year {1}!", treasure, year);
+
+                        if (treasure <= 0 || treasure >= 1000)
+                            Console.WriteLine("Game Over.");
+                    }
+                }
+                else if (gameType == 2) //game 2 - loop based on number of years indicated by user
+                {
+                    //capture user input
+                    Console.WriteLine("Arghhh! How many years would you like to go through?");
+                    int duration = Convert.ToInt32(Console.ReadLine());
+
+                    while (year <= duration)
+                    {
+                        if (r.Next(0, 2) == 0)
+                        {
+                            treasure = treasure + 25;//increase gold amount
+                        }
+                        else
+                        {
+                            treasure = treasure - 25;//decrease gold amount
+                        }
+
+                        year++;//increment year
+                               //display updated gold amount plus year
+                        Console.WriteLine("You have {0} gold and it is year {1}!", treasure, year);
+                    }
+                }
+                else //retired
+                    isActive = false;
+
+                if (!isActive)//Game ends when user has 0 gold
+                    Console.WriteLine("Game Over.");
+            }
+            catch
+            {
+                Console.WriteLine("Error. Please input a valid integer next time pirates!");
+            }
+
+        }//M
+    }//C
+}//NS
---
+++ Assignment6.cs
@@ -0,0 +1,79 @@
+﻿/* Sidney Rood
+ * Assignment 6 - IS350
+ * Program will simulate the war card game
+ * Consists of two players and uses methods to have them play war until there is a winner
+ */
+
+using System.Reflection.Metadata.Ecma335;
+
+namespace Assignment6
+{
+    internal class Assignment6
+    {
+        static void Main(string[] args)
+        {
+            //Welcome message and prompt user for players names
+            Console.WriteLine("Welcome to War! Don't worry just the card game!");
+            Console.WriteLine("Enter player 1's name:");
+            string p1Name = Console.ReadLine();
+            Console.WriteLine("Enter player 2's name:");
+            string p2Name = Console.ReadLine();
+
+            War(p1Name, p2Name); //Call war method
+
+        }//Main Method
+
+        static int Draw()
+        {
+            Random r = new Random();
+            return r.Next(1, 14);
+        }
+
+        static void War(string p1Name, string p2Name)
+        {
+            //Initial card count
+            int p1CardTotal = 26;
+            int p2CardTotal = 26;
+            int rounds = 0;
+            string winner = "";
+
+            //Loop to play game out
+            while (p1CardTotal > 0 && p2CardTotal > 0)
+            {
+                //let player1 draw a card
+                int p1Draw = Draw();
+                int p2Draw = Draw();
+
+                //If statement for card comparison
+                if (p1Draw > p2Draw) //p1 win with high card
+                {
+                    p1CardTotal++;
+                    p2CardTotal--;
+                    Console.WriteLine("{0} won with {1} over {2}", p1Name, p1Draw, p2Draw);
+                }
+                else if (p2Draw > p1Draw) //p2 win with high card
+                {
+                    p2CardTotal++;
+                    p1CardTotal--;
+                    Console.WriteLine("{0} won with {1} over {2}", p2Name, p2Draw, p1Draw);
+                }
+                else
+                {
+                    continue; //tie, so move to next round 
+                }
+                //Show updated card count every round
+                Console.WriteLine("{0} has {1} cards, and {2} has {3} cards.", p1Name, p1CardTotal, p2Name, p2CardTotal);
+                rounds++;
+            }
+
+            //Display the winner and congragulate them
+            if (p1CardTotal > 0)
+                winner = p1Name;
+            else if (p2CardTotal > 0)
+                winner = p2Name;
+
+            Console.WriteLine("After {0} rounds, {1} claims victory! Great job at War! Thanks for playing.", rounds, winner);
+        }
+
+    }//Class
+}//Namespace
